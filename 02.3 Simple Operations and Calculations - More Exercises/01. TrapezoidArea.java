0a321a2f33b54628cc79c5b5d20ce99d51454f20
ANeshkova <anitowurth@abv.bg>
2020-07-07 20:40:34 +0300

    Small change

--- 02.3 Simple Operations and Calculations - More Exercises/01. TrapezoidArea.java
+++ 02.3 Simple Operations and Calculations - More Exercises/01. TrapezoidArea.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class TrapezoidArea {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         double side1 = Double.parseDouble(scan.nextLine());
         double side2 = Double.parseDouble(scan.nextLine());
--- 02.3 Simple Operations and Calculations - More Exercises/02. TriangleArea.java
+++ 02.3 Simple Operations and Calculations - More Exercises/02. TriangleArea.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class TriangleArea {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         double side = Double.parseDouble(scan.nextLine());
         double high = Double.parseDouble(scan.nextLine());
