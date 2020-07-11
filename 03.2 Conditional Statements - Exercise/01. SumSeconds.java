d54089c1f1c2c6c3a0bf52af11f5cd0e82732e18
ANeshkova <anitowurth@abv.bg>
2020-07-11 22:34:21 +0300

    Added solution

---
+++ 03.2 Conditional Statements - Exercise/01. SumSeconds.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumSeconds {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int firstTime = Integer.parseInt(scan.nextLine());
+        int secondTime = Integer.parseInt(scan.nextLine());
+        int thirdTime = Integer.parseInt(scan.nextLine());
+
+        int totalTime = firstTime + secondTime + thirdTime;
+        int minutes = totalTime / 60;
+        int seconds = totalTime % 60;
+
+        if (seconds < 10) {
+            System.out.printf("%d:0%d", minutes, seconds);
+        } else {
+            System.out.printf("%d:%d", minutes, seconds);
+        }
+    }
+}
